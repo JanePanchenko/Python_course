fdcc323b9ee94b5dd0f2985e3c2d621cba152a24
Yevheniia_Panchenko <yevheniiapanchenko2020@gmail.com>
2024-01-21 18:32:49 +0100

    HomeTask2: Regular calculator + temperature calculator

---
+++ Python_course/home_task_2/calculator.py
@@ -0,0 +1,14 @@
+
+class Calculator:
+
+    def sum(self, number1: float, number2: float) -> float:
+        return number1 + number2
+
+    def subtract(self, number1: float, number2: float) -> float:
+        return number1-number2
+
+    def multiply(self, number1: float, number2:float) -> float:
+        return number1*number2
+
+    def divide(self, number1: float, number2: float) -> float:
+        return number1/number2
---
+++ Python_course/home_task_2/temperature_calculator.py
@@ -0,0 +1,6 @@
+celsius = float(input("Enter the temperature in celsius: "))
+
+fahrenheit = (celsius * 9 / 5 + 32)
+print("Temperature in Fahrenheit: " + str(fahrenheit))
+kelvin = (celsius + 273.15)
+print("Temperature in Kelvin: " + str(kelvin))
